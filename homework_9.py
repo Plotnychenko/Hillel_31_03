27cfb9aec43ec8ff21dd0451685e49d301ae1eb7
Plotnychenko <plotnichenkoedik@gmail.com>
2021-05-16 18:18:09 +0300

    homework_9 final

--- homework_9.py
+++ homework_9.py
@@ -1,16 +1,18 @@
 # Homework 9
 # E. Plotnychenko
-import re
+
 ################################################
 """
 1. Написать функцию, которая получает в виде параметра имя файла названия интернет доменов (domains.txt)
 и возвращает их в виде списка строк (названия возвращать без точки).
 """
-# def create_domains_list(filename):
-#     with open(filename, "r") as txt_files:
-#         domains_list = txt_files.readlines()
-#     new_domains_list = [value.lstrip(".") for value in domains_list]
-#     return new_domains_list
+
+
+def create_domains_list(filename):
+    with open(filename, "r") as txt_files:
+        domains_list = txt_files.readlines()
+    new_domains_list = [value.lstrip(".") for value in domains_list]
+    return new_domains_list
 
 ################################################
 """
@@ -19,12 +21,14 @@ import re
 Каждая строка файла содержит номер, фамилию, страну, некоторое число (таблица взята с википедии).
 Разделитель - символ табуляции "\t"
 """
-# def create_surnames_list(filename):
-#     with open(filename, "r") as txt_files:
-#         object_list = txt_files.readlines()
-#     strings_split_list = [my_str.split("\t") for my_str in object_list]
-#     surnames_list = [surname[1] for surname in strings_split_list]
-#     return surnames_list
+
+
+def create_surnames_list(filename):
+    with open(filename, "r") as txt_files:
+        object_list = txt_files.readlines()
+    strings_split_list = [my_str.split("\t") for my_str in object_list]
+    surnames_list = [surname[1] for surname in strings_split_list]
+    return surnames_list
 
 ################################################
 """
@@ -35,16 +39,39 @@ import re
 Например [{"date_original": "8th February 1828", "date_modified": 08/02/1828},  ...]
 """
 
-def create_list(filename):
-    data_list = []
+
+def create_data_original_list(filename):
     with open(filename, "r") as txt_files:
         info_list = txt_files.readlines()
-    info_list = [value.split("-") for value in info_list]
-    for info in info_list:
-        for value in info:
-            if len(info) > 1:
-                data_list.append(value.split())
-    return data_list
+    info_list = [value.split(" - ")[0].split() for value in info_list if " - " in value]
+    data_original_list = [value for value in info_list if len(value) == 3]
+    return data_original_list
+
+
+def create_data_modified_list(filename):
+    data_mod_list = create_data_original_list(filename)
+    for day in data_mod_list:
+        day[0] = day[0].rstrip("nsthrd")
+        if len(day[0]) == 1:
+            day[0] = "0" + day[0]
+        day[1] = month_dict[day[1]]
+        data_modified_list = ["/".join(day) for day in data_mod_list]
+    return data_modified_list
+
+
+def create_list_data_dict(filename):
+    list_data_dict = []
+    data_original_list = create_data_modified_list(filename)
+    data_origin_list = [" ".join(data) for data in data_original_list]
+    data_modified_list = create_data_modified_list(filename)
+    index = 0
+    while index < len(data_original_list):
+        dict_data = {"data_original": data_origin_list[index], "data_modified": data_modified_list[index]}
+        list_data_dict.append(dict_data)
+        index += 1
+    return list_data_dict
+
+
 month_dict = {"January": "01",
               "February": "02",
               "March": "03",
@@ -59,31 +86,12 @@ month_dict = {"January": "01",
               "December": "12"
               }
 
+filename = "domains.txt"
+domains_list = create_domains_list(filename)
+
+filename = "names.txt"
+surnames_list = create_surnames_list(filename)
+
 filename = "authors.txt"
-info_list = create_list(filename)
-print(info_list)
-
-
-
-#
-# # Вероятно, изобретаю велосипед
-# def date_transform(filename):
-#     date_list = strip_strings(filename)
-#     ddmmyyyy_list = [value.split() for value in date_list]
-#     return ddmmyyyy_list
-#     # def transform(day):
-#
-#     return ddmmyyyy_list
-
-
-
-
-
-
-
-
-# filename = "domains.txt"
-# domains_list = create_domains_list(filename)
-
-# filename = "names.txt"
-# surnames_list = create_surnames_list(filename)
+data_dict = create_list_data_dict(filename)
+print(data_dict)
