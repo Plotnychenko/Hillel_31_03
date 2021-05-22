740f1612ce06a4f7b5b3b3cc23387327b4b008ec
Plotnychenko <plotnichenkoedik@gmail.com>
2021-05-22 20:04:19 +0300

    homework_11

--- homework_11.py
+++ homework_11.py
@@ -23,40 +23,29 @@ def file_reader(filename):
 
 
 # 2
-def sort_surname(filename):
-    famous_math_list = file_reader(filename)
-    surname_list = [famous_math_dict["name"].split()[-1] for famous_math_dict in famous_math_list]
+def sort_surname(dict_list):
+    surname_list = []
+    surname_list.append(dict_list.get("name").split()[-1])
     return surname_list
 
 
 # 3
-def sort_by_dday(filename):
-    famous_list = file_reader(filename)
-    years_list = [int((re.findall(r'[0-9]+', famous_math_dict["years"])[1])) for famous_math_dict in famous_list]
-    for index, math_dict in enumerate(famous_list):
-        if "BC" in math_dict["years"]:
-            years_list[index] = -1 * years_list[index]
-    return years_list
+def sort_by_dday(data_list):
+    life_years = data_list.get("years")
+    years_string = re.findall(r'[0-9]+', life_years)
+    years_int = [int(year) for year in years_string]
+    death_year = max(years_int) if "BC" not in years_int else -1 * min(life_years)
+    return death_year
 
 
-def sort_by_dday_var_2(filename):
-    famous_list = file_reader(filename)
-    years_list = [int((re.findall(r'[0-9]+', famous_math_dict["years"])[1])) for famous_math_dict in famous_list]
-    for index, math_dict in enumerate(famous_list):
-        if "BC" in math_dict["years"]:
-            years_list[index] = -1 * years_list[index]
-    death_data_list = [abs(year) for year in sorted(years_list)]
-    return death_data_list
-
-# 4
-def sort_len_text(filename):
-    data_list = file_reader(filename)
-    text_list = [text["text"].split() for text in data_list]
-    return text_list
+# # 4
+def sort_len_text(data_list):
+    info_text = data_list.get("text")
+    return len(info_text)
 
 
 filename = "data.json"
-name_sort = sorted(sort_surname(filename))
-death_data_sort = sorted(sort_by_dday(filename))
-death_data_sort_2 = sort_by_dday_var_2(filename)
-text_len_sort = sorted(sort_len_text(filename), key=len)
+data_list = file_reader(filename)
+name_sort = sorted(data_list, key=sort_surname)
+death_data_sort = sorted(data_list, key=sort_by_dday)
+text_len_sort = sorted(data_list, key=sort_len_text)
