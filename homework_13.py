f758017a22ebc7590e8cc439761ae971f4d747fb
Plotnychenko <plotnichenkoedik@gmail.com>
2021-06-01 22:41:06 +0300

    homework_13 refactor

--- homework_13.py
+++ homework_13.py
@@ -2,9 +2,11 @@
 # E. Plotnychenko
 
 class FileWorker:
-    def __init__(self, filename):
-        self.filename = filename
-        self.month_dict = {"January": "01",
+    def __init__(self, authors, domains, names):
+        self._authors = self.file_reader(authors)
+        self._domains = self.file_reader(domains)
+        self._names = self.file_reader(names)
+        self._month_dict = {"January": "01",
                            "February": "02",
                            "March": "03",
                            "April": "04",
@@ -15,56 +17,52 @@ class FileWorker:
                            "September": "09",
                            "October": "10",
                            "November": "11",
-                           "December": "12"
-                           }
+                            "December": "12"
+                            }
         self.format_domains_list = []
-        self.surnames_list = []
-        self.list_date_dict = {}
-        self.file_reader()
+        self._surnames_list = []
 
-    def file_reader(self):
-        with open(self.filename, "r") as txt_files:
+    def file_reader(self, filename):
+        with open(filename, "r") as txt_files:
             return txt_files.readlines()
 
-    def create_domains_list(self):
-        domains_list = self.file_reader()
-        format_domains_list = [value.lstrip(".") for value in domains_list]
+    @property
+    def domains_list(self):
+        format_domains_list = [value.lstrip(".") for value in self._domains]
         return format_domains_list
 
-    def create_surnames_list(self):
-        object_list = self.file_reader()
-        strings_split_list = [my_str.split("\t") for my_str in object_list]
-        self.surnames_list = [surname[1] for surname in strings_split_list]
-        return self.surnames_list
+    @property
+    def surnames_list(self):
+        strings_split_list = [my_str.split("\t") for my_str in self._names]
+        self._surnames_list = [surname[1] for surname in strings_split_list]
+        return self._surnames_list
 
-    def create_date_original_list(self):
-        info_list = self.file_reader()
-        info_list = [value.split(" - ")[0].split() for value in info_list if " - " in value]
-        date_original_list = [value for value in info_list if len(value) == 3]
-        return date_original_list
+    def _create_date_original_list(self):
+        info_list = [value.split(" - ")[0].split() for value in self._authors if " - " in value]
+        _date_original_list = [value for value in info_list if len(value) == 3]
+        return _date_original_list
 
-    def modified_date(self, date):
+    def _modified_date(self, date):
         date[0] = date[0].rstrip("nsthrd")
         if len(date[0]) == 1:
             date[0] = "0" + date[0]
-        date[1] = self.month_dict[date[1]]
+        date[1] = self._month_dict[date[1]]
         return "/".join(date)
 
-    def create_list_date_dict(self):
-        list_date_dict = []
-        date_original_list = self.create_date_original_list()
+    @property
+    def list_date_dict(self):
+        _list_date_dict = []
+        date_original_list = self._create_date_original_list()
         index = 0
         while index < len(date_original_list):
             original_date = " ".join(date_original_list[index])
-            dict_date = {"data_original": original_date, "data_modified": self.modified_date(date_original_list[index])}
-            list_date_dict.append(dict_date)
+            dict_date = {"data_original": original_date, "data_modified": self._modified_date(date_original_list[index])}
+            _list_date_dict.append(dict_date)
             index += 1
-        return list_date_dict
+        return _list_date_dict
 
 
-author_worker = FileWorker("authors.txt")
-list_data_dict = author_worker.create_list_date_dict()
-domains_worker = FileWorker("domains.txt")
-domains = domains_worker.create_domains_list()
-names_worker = FileWorker("names.txt")
-surnames = names_worker.create_surnames_list()
+file_worker = FileWorker("authors.txt", "domains.txt", "names.txt")
+print(file_worker.surnames_list)
+print(file_worker.list_date_dict)
+print(file_worker.domains_list)
